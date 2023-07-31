f951f8489c2d7dcbf5152ce651ed3404822e0aa2
oeyoews <jyao4783@gmail.com>
2023-07-31 16:26:51 +0800

    Automatic commit

--- app/articles/layout.tsx
+++ app/articles/layout.tsx
@@ -2,6 +2,8 @@ import ArticleInfo from "components/ArticleInfo";
 import { headers } from "next/headers";
 import Image from "next/image";
 import { getArticleBySlug } from "utils/getArticles";
+import { FiArrowLeft } from "react-icons/fi";
+import Link from "next/link";
 
 export default async function Layout({ children }: { children: React.ReactNode }) {
   const slug = headers().get("x-next-article-slug") as string;
@@ -24,6 +26,12 @@ export default async function Layout({ children }: { children: React.ReactNode }
       }
       <ArticleInfo article={article} className="-mt-8 inline px-1 text-sm" />
       {children}
+      <Link className="flex justify-end no-underline" href="/">
+        <button className="inline transform justify-center rounded-md bg-neutral-200 px-2 py-1 transition duration-300 hover:scale-105">
+          <FiArrowLeft className="inline" />
+          back home
+        </button>
+      </Link>
     </article>
   );
 }
--- components/NavBar.tsx/index.tsx
+++ components/NavBar.tsx/index.tsx
@@ -1,9 +1,12 @@
 import Link from "next/link";
+import { FiHome } from "react-icons/fi";
 
 function NavBar() {
   return (
     <div className="mx-auto flex max-w-5xl justify-end space-x-2 p-2">
-      <Link href="/">Home</Link>
+      <Link href="/">
+        <FiHome className="inline transform text-xl transition duration-200 hover:scale-125" />
+      </Link>
     </div>
   );
 }
