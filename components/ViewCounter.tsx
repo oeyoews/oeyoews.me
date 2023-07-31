367977fa17fd5b14e877b27418a68d85e9e5048b
oeyoews <jyao4783@gmail.com>
2023-07-31 18:59:25 +0800

    fix: slug is obe props

--- components/ViewCounter.tsx
+++ components/ViewCounter.tsx
@@ -1,8 +1,9 @@
 import { kv } from "@vercel/kv";
 import { FiEye } from "react-icons/fi";
 
-async function ViewCounter(slug) {
+async function ViewCounter({ slug }) {
   const views = await kv.incr(slug);
+  const test = await kv.get(slug);
 
   return (
     <>
