f5093a2950d00acbdf372dbedf950bb2390f85a9
oeyoews <jyao4783@gmail.com>
2023-08-01 08:58:26 +0800

    feat: Hitokoto use server components

--- components/Hitokoto.tsx
+++ components/Hitokoto.tsx
@@ -1,30 +1,16 @@
-"use client";
-
-import { Suspense, useEffect, useState } from "react";
-
-function Hitokoto() {
-  const [hitokotoText, setHitokotoText] = useState("");
-
-  async function fetchHitokotoText() {
-    const response = await fetch("https://v1.hitokoto.cn");
-    const { hitokoto: hitokotoText } = await response.json();
-    setHitokotoText(hitokotoText);
-  }
-
-  useEffect(() => {
-    fetchHitokotoText();
-  }, []);
+async function Hitokoto() {
+  const response = await fetch("https://v1.hitokoto.cn", {
+    cache: "no-store",
+  });
+  const { hitokoto: hitokotoText } = await response.json();
 
   return (
-    <Suspense fallback={<div>Loading ...</div>}>
-      <div
-        className="mt-8 select-none rounded-sm bg-gradient-to-r from-red-300 via-purple-400 to-blue-500 bg-clip-text p-1 text-center text-sm text-transparent hover:cursor-pointer"
-        title="点击刷新一言"
-        onClick={fetchHitokotoText}
-      >
-        {hitokotoText}
-      </div>
-    </Suspense>
+    <div
+      className="mt-8 select-none rounded-sm bg-gradient-to-r from-red-300 via-purple-400 to-blue-500 bg-clip-text p-1 text-center text-sm text-transparent hover:cursor-pointer"
+      title="点击刷新一言"
+    >
+      {hitokotoText}
+    </div>
   );
 }
 
