a1931cd1e0c1dcbcece3fb87f52047c11773c323
oeyoews <jyao4783@gmail.com>
2023-08-01 22:04:05 +0800

    feat: support code highlight with bright

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,16 +1,10 @@
 import type { MDXComponents } from "mdx/types";
+import { Code } from "bright";
 
-// This file allows you to provide custom React components
-// to be used in MDX files. You can import and use any
-// React component you want, including components from
-// other libraries.
-
-// This file is required to use MDX in `app` directory.
-export function useMDXComponents(components: MDXComponents): MDXComponents {
+export function useMDXComponents(components: MDXComponents) {
+  Code.lineNumbers = false;
+  Code.theme = "one-dark-pro";
   return {
-    // Allows customizing built-in components, e.g. to add styling.
-    // h1: ({ children }) => <h1 style={{ fontSize: "100px" }}>{children}</h1>,
-    ...components,
-    // code: SyntaxHighlight,
+    pre: Code,
   };
 }
