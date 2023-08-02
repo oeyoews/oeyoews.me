2d52ab3bfa08641e3bf96ece2dab95e278c693de
oeyoews <jyao4783@gmail.com>
2023-08-02 18:18:59 +0800

    fix: revert to params

--- app/api/counter/route.ts
+++ app/api/counter/route.ts
@@ -3,15 +3,14 @@ import { NextResponse } from 'next/server';
 import { kv } from '@vercel/kv';
 
 export async function GET(request: Request) {
-  // const { searchParams } = new URL(request.url);
-  // const user = searchParams.get('user');
-  // if (!user) return NextResponse.json({ message: 'No params' });
+  const { searchParams } = new URL(request.url);
+  const user = searchParams.get('user');
+  if (!user) return NextResponse.json({ message: 'No params' });
   let originalUser;
-  originalUser = await kv.incr('oeyoews');
-  // if (user === 'oeyoews') {
-  //   originalUser = await kv.incr(user);
-  // } else {
-  //   originalUser = 'The user is not found';
-  // }
+  if (user === 'oeyoews') {
+    originalUser = await kv.incr(user);
+  } else {
+    originalUser = 'The user is not found';
+  }
   return NextResponse.json({ oeyoews: { quantity: originalUser } });
 }
