ad4546661eee20b3328143e28f17234c115d5912
oeyoews <jyao4783@gmail.com>
2023-08-02 13:20:21 +0800

    Automatic commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import AllArticles from '@/components/AllArticles';
-import ViewCounter from '@/components/ViewCounter';
 
 import { isDev } from '@/lib/isDev';
 
@@ -8,9 +7,6 @@ export default async function Page() {
     <div className="m-4 sm:m-8">
       <div className="mx-auto max-w-xl">
         <AllArticles />
-        <div className="my-4 text-center text-sm text-gray-500">
-          {!isDev && <ViewCounter slug="home" />}
-        </div>
       </div>
     </div>
   );
