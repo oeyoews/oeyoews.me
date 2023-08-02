ed126eee5d7c9021c4aa1f14f78e6422cf6c7566
oeyoews <jyao4783@gmail.com>
2023-08-02 17:55:15 +0800

    Automatic commit

--- app/api/counter/route.ts
+++ app/api/counter/route.ts
@@ -3,14 +3,15 @@ import { NextResponse } from 'next/server';
 import { kv } from '@vercel/kv';
 
 export async function GET(request: Request) {
-  const { searchParams } = new URL(request.url);
-  const user = searchParams.get('user');
-  if (!user) return NextResponse.json({ message: 'No params' });
+  // const { searchParams } = new URL(request.url);
+  // const user = searchParams.get('user');
+  // if (!user) return NextResponse.json({ message: 'No params' });
   let originalUser;
-  if (user === 'oeyoews') {
-    originalUser = await kv.incr(user);
-  } else {
-    originalUser = 'The user is not found';
-  }
+  originalUser = await kv.get('oeyoews');
+  // if (user === 'oeyoews') {
+  //   originalUser = await kv.incr(user);
+  // } else {
+  //   originalUser = 'The user is not found';
+  // }
   return NextResponse.json({ oeyoews: { quantity: originalUser } });
 }
