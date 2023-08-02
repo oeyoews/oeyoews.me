d7705aaffe759d2da1abd41c33e3bda334105e11
oeyoews <jyao4783@gmail.com>
2023-08-02 18:33:04 +0800

    Automatic commit

--- app/api/counter/route.ts
+++ app/api/counter/route.ts
@@ -2,7 +2,12 @@ import { NextResponse } from 'next/server';
 
 import { kv } from '@vercel/kv';
 
+// if no params, on dev is normal, but on prod is original value, if have params, all will be worked weired ???
+// but if have params, in frond to fetch is failed, only no params is worked
 export async function GET(request: Request) {
+  // const originalUser = await kv.incr('oeyoews');
+  // const d = originalUser;
+
   const { searchParams } = new URL(request.url);
   const user = searchParams.get('user');
   if (!user) return NextResponse.json({ message: 'No params' });
