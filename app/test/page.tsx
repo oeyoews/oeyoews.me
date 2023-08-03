16bd9e0be44c0f0230c4f65aaebc4446d57c7fb5
oeyoews <jyao4783@gmail.com>
2023-08-04 00:24:28 +0800

    Automatic commit

--- app/test/page.tsx
+++
@@ -1,8 +0,0 @@
-// 'use client';
-import HitCounter from '@/components/HitCounter';
-
-function page() {
-  return <HitCounter />;
-}
-
-export default page;
---
+++ components/Sparkles.tsx
@@ -0,0 +1,4 @@
+//
+// https://www.joshwcomeau.com/react/animated-sparkles-in-react/
+
+// https://github.com/kmjennison/react-sparkle
