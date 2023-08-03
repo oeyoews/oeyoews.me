9863a88952252bbb256b0e3882fc92f0ef2f0d2a
oeyoews <jyao4783@gmail.com>
2023-08-03 11:38:24 +0800

    Automatic commit

--- app/articles/loading.tsx
+++ app/articles/loading.tsx
@@ -1,6 +1,6 @@
 function Loading() {
   return (
-    <div className="mx-auto max-w-5xl font-bold min-h-screen text-center">
+    <div className="mx-auto max-w-5xl font-bold min-h-screen text-center mt-4">
       loading ...
     </div>
   );
