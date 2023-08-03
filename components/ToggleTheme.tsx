bedf31a85ffd3381961258225cf12b33231aa16a
oeyoews <jyao4783@gmail.com>
2023-08-03 15:00:42 +0800

    Automatic commit

--- components/ToggleTheme.tsx
+++ components/ToggleTheme.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Moon, Sun } from 'lucide-react';
+import { BsFillMoonStarsFill, BsFillSunFill, BsSunFill } from 'react-icons/bs';
 
 import { useTheme } from 'next-themes';
 
@@ -19,8 +19,8 @@ export default function ModeToggle() {
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant="outline" size="icon">
-          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          <BsSunFill className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-180 dark:scale-0" />
+          <BsFillMoonStarsFill className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
