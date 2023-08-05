987c6982b5c4d546c2d2bd806e5a089073e1a351
oeyoews <jyao4783@gmail.com>
2023-08-05 22:22:30 +0800

    Automatic commit

--- middleware.ts
+++ middleware.ts
@@ -4,6 +4,7 @@ import type { NextRequest } from 'next/server';
 export default function middleware(request: NextRequest) {
   const requestHeaders = new Headers(request.headers);
   if (request.nextUrl.pathname.startsWith('/articles/'))
+    // if (request.url.includes('/articles/'))
     requestHeaders.set(
       'x-next-article-slug',
       request.nextUrl.pathname.replace('/articles/', ''),
