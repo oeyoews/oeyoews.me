65b5822ca9e4239751ba796d6b12a590fd0187bd
oeyoews <jyao4783@gmail.com>
2023-08-07 07:53:50 +0800

    Automatic commit

--- components/ToggleTheme.tsx
+++ components/ToggleTheme.tsx
@@ -22,6 +22,7 @@ export default function ModeToggle() {
         <Button variant="ghost" size="icon">
           <PiSunThin className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-180 dark:scale-0" />
           <PiMoonStarsThin className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          {/* ?? */}
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
