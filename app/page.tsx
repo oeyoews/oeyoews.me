7d011b33e4e9c7867ee1d88c7c3b06cab57afe01
oeyoews <jyao4783@gmail.com>
2023-08-09 14:46:35 +0800

    Automatic commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,18 @@
-import { RiTimeLine } from 'react-icons/ri';
-
 import AllArticles from '@/components/AllArticles';
 import ViewCounter from '@/components/ViewCounter';
 
-export default async function Page() {
+export default function Page() {
   return (
     <div className="m-4 sm:m-8">
       <div className="mx-auto max-w-xl">
         <AllArticles />
         <div className="my-4 text-sm text-center text-gray-400">
           <ViewCounter slug="home" />
-          <div className="my-1">
+          {/* 由于文章不是静态生成, 所以当前路由 是ssr, buildtime 总是会刷新 */}
+          {/* <div className="my-1">
             <RiTimeLine className="inline stroke-0 fill-blue-300" />{' '}
             {new Date().toLocaleString()}
-          </div>
+          </div> */}
         </div>
       </div>
     </div>
