aa562ac897f86870be8a266f1515ec8e1f06ac53
oeyoews <jyao4783@gmail.com>
2023-08-15 18:51:42 +0800

    Automatic commit

--- components/About.tsx
+++
@@ -1,9 +0,0 @@
-function About() {
-  return (
-    <div className="prose prose-indigo mx-auto my-4 max-w-xl">
-      <h1>oeyoews</h1>
-    </div>
-  );
-}
-
-export default About;
--- components/ControlPanel/index.tsx
+++
@@ -1,9 +0,0 @@
-import VaulButton from '../VaulButton';
-
-export default function index() {
-  return (
-    <>
-      <VaulButton buttonText="about" title="About me" />
-    </>
-  );
-}
