b3f8b02735f0643add8124f09b98c3f192feb56f
oeyoews <jyao4783@gmail.com>
2023-08-17 22:10:58 +0800

    Automatic commit

--- components/CalendarPost/index.tsx
+++ components/CalendarPost/index.tsx
@@ -1,8 +1,31 @@
 'use client';
 
-import ReactECharts from 'echarts-for-react';
+import ReactEChartsCore from 'echarts-for-react/lib/core';
 
 import { addYears, endOfMonth, format, startOfMonth, subYears } from 'date-fns';
+import { HeatmapChart } from 'echarts/charts';
+import {
+  CalendarComponent,
+  DatasetComponent,
+  TitleComponent,
+  TooltipComponent,
+  VisualMapComponent,
+} from 'echarts/components';
+import * as echarts from 'echarts/core';
+import {
+  // CanvasRenderer,
+  SVGRenderer,
+} from 'echarts/renderers';
+
+echarts.use([
+  CalendarComponent,
+  HeatmapChart,
+  DatasetComponent,
+  TitleComponent,
+  TooltipComponent,
+  SVGRenderer,
+  VisualMapComponent,
+]);
 
 function CalendarHeatmapComponent({ datas }: { datas: any[] }) {
   const postCounts = {}; // 统计每天的帖子数目
@@ -20,7 +43,7 @@ function CalendarHeatmapComponent({ datas }: { datas: any[] }) {
   });
 
   const data = Object.entries(postCounts).map(([date, count]) => {
-    return { name: date, value: [new Date(date), count] };
+    return { name: date, value: [new Date(date), `${count}`] };
   });
 
   const currentDate = new Date();
@@ -34,8 +57,15 @@ function CalendarHeatmapComponent({ datas }: { datas: any[] }) {
   }
 
   const option = {
+    title: {
+      text: `共有 ${datas.length} 篇文章`,
+      left: 'center',
+      margin: 8,
+      position: 'bottom',
+    },
     tooltip: {
       position: 'top',
+      // formatter:
     },
     visualMap: {
       type: 'piecewise',
@@ -47,30 +77,47 @@ function CalendarHeatmapComponent({ datas }: { datas: any[] }) {
       pieces: [
         // 设置分段范围
         { lte: 0, color: '#EBEDF0' },
-        { gt: 0, lte: 1, color: '#0E4429' },
+        { gt: 0, lte: 1, color: '#39D353' },
         { gt: 2, lte: 4, color: '#006D32' },
         { gt: 5, lte: 15, color: '#26A641' },
-        { gt: 16, color: '#39D353' },
+        { gt: 16, color: '#0E4429' },
       ],
     },
     calendar: {
+      emptyItemStyle: {
+        color: 'red',
+        borderWidth: 0.5, // 可根据需要调整
+        borderType: 'dashed', // 可根据需要调整
+      },
       top: 60,
       left: 0,
       right: 0,
-      cellSize: [15, 15],
-      orient: 'horizontal',
+      cellSize: 15,
+      orient: 'horizontal', // vertical(not fit)
       range: [startDate, endDate],
+      splitLine: {
+        show: false,
+        lineStyle: {
+          color: ['#ccc'],
+          width: 1,
+          type: 'solid',
+        },
+      },
       itemStyle: {
-        borderWidth: 0.5,
+        borderWidth: 3,
         borderCap: 'round',
         borderJoin: 'round',
-        borderColor: '#ccc',
+        borderColor: 'white',
+        color: '#EBEDF0',
       },
       yearLabel: {
         show: true,
-        position: 'top',
+        position: 'bottom',
         margin: 32,
         verticalAlign: 'top',
+      },
+      dayLabel: {
+        margin: 15,
       },
     },
     series: [
@@ -82,7 +129,7 @@ function CalendarHeatmapComponent({ datas }: { datas: any[] }) {
     ],
   };
 
-  return <ReactECharts option={option} />;
+  return <ReactEChartsCore echarts={echarts} option={option} />;
 }
 
 export default CalendarHeatmapComponent;
