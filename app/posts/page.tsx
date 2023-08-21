4a4b7233bd59d73e063c4a95ec3d1785c8d53ec1
oeyoews <jyao4783@gmail.com>
2023-08-21 22:21:22 +0800

    Automatic commit

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -25,7 +25,10 @@ export default function HomePage() {
   ];
 
   return (
-    <ol className="prose relative list-none border-gray-100/80 border-l-4">
+    <ol
+      className="prose relative list-none border-gray-100/80 border-l-4"
+      key={'all-posts'}
+    >
       {allPosts
         .sort((a, b) => {
           return a.date > b.date ? -1 : 1;
@@ -56,40 +59,38 @@ export default function HomePage() {
           currentYear = postYear;
 
           return (
-            <>
-              <article key={post._id}>
-                {yearHeader}
-                <li className="ml-6 group my-8">
-                  <span className="absolute flex items-center justify-center w-6 h-6 rounded-full -left-3 bg-white">
-                    <FcFolder className="group-hover:hidden h-4 w-4 text-gray-400 duration-300 transition-all group-hover:stroke-indigo-500" />
-                    <FcOpenedFolder className="hidden group-hover:block h-4 w-4 text-gray-400 duration-300 transition-all group-hover:stroke-indigo-500" />
-                  </span>
-                  <Link
-                    href={post.slug}
-                    className="text-xs rounded-md"
-                    title="点击阅读更多"
-                  >
-                    <h2 className="text-neutral-700 hover:text-neutral-950 duration-300 transition my-2">
-                      {post.title}
-                      {post.password && (
-                        // <FcLock className="w-3 h-3 inline-flex ml-1" />
-                        <sup className="bg-purple-200 rounded-sm text-xs px-1 mx-1 font-light inline-flex text-gray-800 font-serif">
-                          password
-                        </sup>
-                      )}
-                      {post.draft === true && (
-                        <sup className="mx-1 bg-gray-300 rounded-sm px-1 font-serif font-thin">
-                          draft
-                        </sup>
-                      )}
-                    </h2>
-                  </Link>
-                  <time className="block text-sm font-normal leading-none text-gray-400">
-                    {format(new Date(post.date), 'EEE, MMMM d')}
-                  </time>
-                </li>
-              </article>
-            </>
+            <article key={post._id}>
+              {yearHeader}
+              <li className="ml-6 group my-8">
+                <span className="absolute flex items-center justify-center w-6 h-6 rounded-full -left-3 bg-white">
+                  <FcFolder className="group-hover:hidden h-4 w-4 text-gray-400 duration-300 transition-all group-hover:stroke-indigo-500" />
+                  <FcOpenedFolder className="hidden group-hover:block h-4 w-4 text-gray-400 duration-300 transition-all group-hover:stroke-indigo-500" />
+                </span>
+                <Link
+                  href={post.slug}
+                  className="text-xs rounded-md"
+                  title="点击阅读更多"
+                >
+                  <h2 className="text-neutral-700 hover:text-neutral-950 duration-300 transition my-2">
+                    {post.title}
+                    {post.password && (
+                      // <FcLock className="w-3 h-3 inline-flex ml-1" />
+                      <sup className="bg-purple-200 rounded-sm text-xs px-1 mx-1 font-light inline-flex text-gray-800 font-serif">
+                        password
+                      </sup>
+                    )}
+                    {post.draft === true && (
+                      <sup className="mx-1 bg-gray-300 rounded-sm px-1 font-serif font-thin">
+                        draft
+                      </sup>
+                    )}
+                  </h2>
+                </Link>
+                <time className="block text-sm font-normal leading-none text-gray-400">
+                  {format(new Date(post.date), 'EEE, MMMM d')}
+                </time>
+              </li>
+            </article>
           );
         })}
       <CommitInfo />
