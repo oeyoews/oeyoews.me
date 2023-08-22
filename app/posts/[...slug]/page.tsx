fb8d7f74ede7c48663302d7b250d9e7e295328d3
oeyoews <jyao4783@gmail.com>
2023-08-22 12:29:24 +0800

    Automatic commit

--- app/posts/[...slug]/page.tsx
+++ app/posts/[...slug]/page.tsx
@@ -53,7 +53,7 @@ export default async function PostPage({ params }: PostProps) {
 
   return (
     <article className="py-6 prose prose-img:rounded-md">
-      <h1 className="mb-2">{post.title}</h1>
+      <h1 className="mb-2 text-3xl">{post.title}</h1>
       <hr className="my-4 border-2 border-gray-100 rounded-full" />
       <PasswordProtectedContent post={post}>
         <Mdx code={post.body.code} />
