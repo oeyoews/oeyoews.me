4876e5e950f5eb640a60d979034f51bfcf43eb54
oeyoews <jyao4783@gmail.com>
2023-08-25 01:02:54 +0800

    Automatic commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,6 @@
-import dynamic from 'next/dynamic';
+import HeatmapComponent from '@/components/CalendarPost';
 
 import { allPosts } from '@/.contentlayer/generated';
-
-const HeatmapComponent = dynamic(() => import('@/components/CalendarPost'), {
-  ssr: false,
-  loading: () => <div className="text-center font-semibold">Loading ...</div>,
-});
 
 function page() {
   return <HeatmapComponent datas={allPosts} />;
