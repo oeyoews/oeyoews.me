2ba81cfa34a7b58c4058eda58058ced7d6976d4e
oeyoews <jyao4783@gmail.com>
2023-08-25 19:39:09 +0800

    Automatic commit

--- components/PostList/game/Square.tsx
+++ components/PostList/game/Square.tsx
@@ -3,6 +3,7 @@ interface SquareProps {
 	onSquareClick: () => void;
 }
 
+// TODO: support mouse hover to prevew next square
 function Square({ value, onSquareClick }: SquareProps) {
 	return (
 		<button className="border border-white font-bold w-16 h-16" onClick={onSquareClick}>
