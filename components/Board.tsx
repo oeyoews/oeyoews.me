57fc4b80d155914e63afe42088479f0b3b563307
oeyoews <jyao4783@gmail.com>
2023-08-25 19:01:10 +0800

    Automatic commit

--- components/Board.tsx
+++ components/Board.tsx
@@ -2,6 +2,7 @@
 
 import { useState } from 'react';
 
+import calculateWinner from '@/lib/calculateWinner';
 import confetti from 'canvas-confetti';
 import sound from 'use-sound';
 
@@ -137,32 +138,4 @@ export default function Board() {
       )}
     </div>
   );
-}
-
-function calculateWinner(squares: any) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (
-      squares[a] &&
-      squares[b] &&
-      squares[c] &&
-      squares[a].props.color === squares[b].props.color &&
-      squares[a].props.color === squares[c].props.color
-    ) {
-      return squares[a].props.color;
-    }
-  }
-
-  return null;
 }
