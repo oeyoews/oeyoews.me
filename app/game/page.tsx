e12448021d4203af8b9e6616657707116568d50f
oeyoews <jyao4783@gmail.com>
2023-09-16 09:40:28 +0800

    perf: remove game

--- app/game/page.tsx
+++
@@ -1,9 +0,0 @@
-import Board from '@/components/PostList/game/Board';
-
-export default function page() {
-  return (
-    <div className="fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
-      <Board />
-    </div>
-  );
-}
