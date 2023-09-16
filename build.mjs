f470a17ccd1a4d96cd46d63d33e1a3662d010a53
oeyoews <jyao4783@gmail.com>
2023-09-16 09:59:30 +0800

    Automatic commit

--- build.mjs
+++ build.mjs
@@ -2,7 +2,7 @@ import { spawn } from 'child_process';
 import ci from 'ci-info';
 
 if (ci.isCI) {
-  spawn('pnpm', ['tiged'], ['oeyoews/nextjs-mdx-blog-content'], ['content'], {
+  spawn('pnpm', ['tiged', 'oeyoews/nextjs-mdx-blog-content', 'content'], {
     stdio: 'inherit',
   }).on('close', () => {
     spawn('pnpm', ['next', 'build'], { stdio: 'inherit' });
