ca839dbba4b0318f15078865120361f1ccceb9bc
oeyoews <jyao4783@gmail.com>
2023-11-06 18:05:43 +0800

    feat: add force-cache

--- lib/getTiddlerData.ts
+++ lib/getTiddlerData.ts
@@ -2,7 +2,9 @@ import formattedTime from './formattedTime';
 import slugifyTitle from './slugifyTitle';
 
 export default async function getTiddlerData() {
-  const res = await fetch('https://neotw.oeyoewl.top/markdown.json');
+  const res = await fetch('https://neotw.oeyoewl.top/markdown.json', {
+    cache: 'force-cache',
+  });
   if (!res.ok) {
     throw new Error('Failed to fetch data');
   }
--- lib/slugifyTitle.ts
+++ lib/slugifyTitle.ts
@@ -2,12 +2,16 @@ import slugify from 'slugify';
 
 export default function slugifyTitle(title: string) {
   slugify.extend({ '/': '-' });
-  return slugify(title, {
+  const slugtitle = slugify(title, {
     replacement: '-', // replace spaces with replacement character, defaults to `-`
     lower: true,
-    strict: true, // strip special characters except replacement, defaults to `false`, remove some unicode
+    strict: true, // strip special characters except replacement, defaults to `false`, remove some unicode, 中文也会被移除
     remove: /[*+~.()'"!:@]/g, // 这里不能正则替换, 仅仅提供了移除
     locale: 'zh',
     trim: true,
   });
+  if (!slugtitle.length) {
+    return Math.random().toString(36).slice(2); // 刷新后就变化了
+  }
+  return slugtitle;
 }
