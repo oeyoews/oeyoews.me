784e7665635fffb2f229895a048a3439a45894f7
oeyoews <jyao4783@gmail.com>
2023-11-07 07:58:05 +0800

    fix #3

--- app/tiddlers/[slug]/page.tsx
+++ app/tiddlers/[slug]/page.tsx
@@ -4,6 +4,14 @@ import Tiddler from '@/components/Tiddler';
 
 import getTiddlerData from '@/lib/getTiddlerData';
 
+export async function generateStaticParams() {
+  const tiddlers = await getTiddlerData();
+
+  return tiddlers.map((tiddler) => ({
+    slug: tiddler.slug,
+  }));
+}
+
 export default async function Page({ params }: { params: { slug: string } }) {
   const { slug } = params;
   const data: Tiddler[] = await getTiddlerData();
