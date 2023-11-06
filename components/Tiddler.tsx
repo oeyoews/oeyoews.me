69d9656fdf5d78165980f96c6f0d223fd03cb993
oeyoews <jyao4783@gmail.com>
2023-11-06 17:07:20 +0800

    format code

--- components/Tiddler.tsx
+++ components/Tiddler.tsx
@@ -1,6 +1,5 @@
 import Markdown from 'react-markdown';
 
-import formattedTime from '@/lib/formattedTime';
 import { Code } from 'bright';
 // @ts-ignore
 import remarkContainer from 'remark-custom-container';
