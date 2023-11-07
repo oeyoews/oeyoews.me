315dc7f619fe018da1f895873796024d384c8bc3
oeyoews <jyao4783@gmail.com>
2023-11-07 09:52:51 +0800

    fix #9

--- lib/formattedTime.ts
+++ lib/formattedTime.ts
@@ -3,5 +3,8 @@ export default function formattedTime(created: string) {
   const year = created?.substring(0, 4);
   const month = created?.substring(4, 6);
   const day = created?.substring(6, 8);
-  return `${year}-${month}-${day}`;
+  const hour = created?.substring(8, 10);
+  const minutes = created?.substring(10, 12);
+  const seconds = created?.substring(12, 14);
+  return `${year}-${month}-${day}T${hour}:${minutes}:${seconds}`;
 }
