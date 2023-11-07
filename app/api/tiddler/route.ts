48c179c2ea6363cfb0efda81602fca0a9ba2ba70
oeyoews <jyao4783@gmail.com>
2023-11-07 20:43:16 +0800

    update

--- app/api/tiddler/route.ts
+++ app/api/tiddler/route.ts
@@ -4,7 +4,11 @@
 //   .then((data) => console.log(data));
 
 export async function GET(request: Request) {
-  const res = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+  // const res = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+  // const res = await fetch('http://127.0.0.1:8000/status');
+  const res = await fetch(
+    'https://neotw.oeyoewl.top/library/recipes/library/tiddlers.json',
+  );
   const data = await res.json();
   return new Response(JSON.stringify(data), {
     headers: {
--- next.config.js
+++ next.config.js
@@ -2,6 +2,27 @@ const { withContentlayer } = require('next-contentlayer');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '*' }, // replace this your actual origin
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET,DELETE,PATCH,POST,PUT',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value:
+              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+          },
+        ],
+      },
+    ];
+  },
   async rewrites() {
     return [
       // 似乎/不起作用 , 后面需要加名字
