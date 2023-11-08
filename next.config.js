230a1910eceb0cb29cbe5d4419b37eaa60cce63d
oeyoews <jyao4783@gmail.com>
2023-11-08 10:25:39 +0800

    update

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
     return [
       {
         // matching all API routes
-        source: '/api/:path*',
+        source: '/:path*',
         headers: [
           { key: 'Access-Control-Allow-Credentials', value: 'true' },
           { key: 'Access-Control-Allow-Origin', value: '*' }, // replace this your actual origin
