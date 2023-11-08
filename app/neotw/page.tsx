2b78359f9f9de92eb0c8c034147eb69b243f8da2
oeyoews <jyao4783@gmail.com>
2023-11-08 10:22:23 +0800

    feat: add metadata

--- app/neotw/page.tsx
+++ app/neotw/page.tsx
@@ -1,7 +1,16 @@
+import { type Metadata } from 'next';
+
 import CalendarHeatmapComponent from '@/components/CalendarPost';
 import TiddlersList from '@/components/TiddlersList';
 
 import getTiddlerJsonData from '@/lib/getTiddlerJsonData';
+
+export function generateMetadata(): Metadata {
+  return {
+    title: 'local Tiddlers',
+    description: 'local tiddlers',
+  };
+}
 
 export default async function TiddlersHomepage() {
   const tiddlers = await getTiddlerJsonData();
--- app/tiddlers/page.tsx
+++ app/tiddlers/page.tsx
@@ -1,7 +1,16 @@
+import { type Metadata } from 'next';
+
 import CalendarHeatmapComponent from '@/components/CalendarPost';
 import TiddlersList from '@/components/TiddlersList';
 
 import getTiddlerData from '@/lib/getTiddlerData';
+
+export function generateMetadata(): Metadata {
+  return {
+    title: 'online Tiddlers',
+    description: 'online tiddlers',
+  };
+}
 
 export default async function TiddlersHomepage() {
   const tiddlers = await getTiddlerData();
