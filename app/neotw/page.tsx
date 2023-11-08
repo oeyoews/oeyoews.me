06f81066584f346d6e673f361d17842c2af05ead
oeyoews <jyao4783@gmail.com>
2023-11-08 09:34:24 +0800

    update

--- app/neotw/page.tsx
+++ app/neotw/page.tsx
@@ -1,143 +1,16 @@
-'use client';
-
-import { useEffect, useState } from 'react';
-import { FcFolder } from 'react-icons/fc';
-
-import Link from 'next/link';
-
 import CalendarHeatmapComponent from '@/components/CalendarPost';
-import Badge from '@/components/PostList/PostBadges';
-import YearHeader from '@/components/PostList/YearHeader';
+import TiddlersList from '@/components/TiddlersList';
 
 import getTiddlerJsonData from '@/lib/getTiddlerJsonData';
-import useStore from '@/lib/store';
-import { format } from 'date-fns';
-import { toast } from 'sonner';
 
-function TiddlerItem({ tiddler, index }: { tiddler: Tiddler; index: number }) {
-  const { title, slug, date } = tiddler;
+export default async function TiddlersHomepage() {
+  const tiddlers = await getTiddlerJsonData();
   return (
-    <li className="ml-6 group my-8">
-      <span className="absolute flex items-center justify-center w-6 h-6 rounded-full -left-3 bg-white">
-        <FcFolder className="h-4 w-4 text-gray-400 duration-300 transition-all group-hover:stroke-indigo-500" />
-      </span>
-      <Link
-        href={`/neotw/${slug}`}
-        className="text-xs rounded-md"
-        title="点击阅读全文"
-      >
-        <h2 className="text-neutral-700 hover:text-neutral-950 duration-300 transition my-2">
-          {title}
-          {index === 0 && (
-            <Badge className="bg-green-300 font-bold" text="Latest" />
-          )}
-        </h2>
-      </Link>
-      <time className="block text-sm font-normal leading-none text-gray-400">
-        {format(date, 'EEE, MMMM d')}
-      </time>
-    </li>
-  );
-}
-
-export default function HomePage() {
-  const tiddlerstore = useStore();
-  const [data, setData] = useState<Tiddler[]>([]);
-  const [initialData, setIntialData] = useState<Tiddler[]>([]);
-  function TiddlersList({ tiddlers }: { tiddlers: Tiddler[] }) {
-    // if (!tiddlers.length) return;
-    let currentYear: number;
-    return (
-      <ol className="prose relative list-none border-gray-100/80 border-l-4">
-        {tiddlers.slice(0, tiddlerstore.loadedItems).map((tiddler, index) => {
-          const { title, date } = tiddler;
-          const postYear = new Date(date).getFullYear();
-
-          const yearHeader =
-            currentYear !== postYear ? (
-              <YearHeader postYear={postYear} />
-            ) : null;
-
-          currentYear = postYear;
-
-          return (
-            <article key={title}>
-              {yearHeader}
-              <TiddlerItem tiddler={tiddler} index={index} />
-            </article>
-          );
-        })}
-      </ol>
-    );
-  }
-
-  const handleLoadMore = () => {
-    if (data.length < tiddlerstore.loadedItems) {
-      toast.error('没有更多了');
-      return;
-    }
-    tiddlerstore.setLoadedItems(tiddlerstore.loadedItems + 30);
-    toast.success('加载成功');
-  };
-
-  const [hasloaded, setHasloaded] = useState(false);
-  const fetchData = () => {
-    const data = getTiddlerJsonData();
-    // @ts-ignore
-    setData(data);
-    // @ts-ignore
-    setIntialData(data);
-    setHasloaded(true);
-  };
-
-  const [searchTerm, setSearchTerm] = useState('');
-  const handleSearchChange = (event: any) => {
-    setSearchTerm(event.target.value);
-  };
-
-  useEffect(() => {
-    if (!searchTerm) {
-      setData(initialData);
-      return;
-    }
-    const filteredData = data.filter((tiddler) =>
-      tiddler.title.toLowerCase().includes(searchTerm.toLowerCase()),
-    );
-    if (!filteredData.length) return;
-    setData(filteredData);
-  }, [searchTerm, data, initialData]);
-
-  useEffect(() => {
-    fetchData();
-  }, []);
-
-  return (
-    hasloaded && (
-      <div>
-        <CalendarHeatmapComponent datas={data} />
-        <input
-          className="w-full focus:ring-2 focus:ring-indigo-500 outline-indigo-400 focus:ring-opacity-50 rounded px-2 font-mono py-1"
-          autoFocus={true}
-          type="text"
-          value={searchTerm}
-          onChange={handleSearchChange}
-          placeholder="Search Tiddlers(local)"
-        />
-        {!data.length && (
-          <div className="font-mono my-4 text-lg flex justify-center items-center">
-            <h1>Nothing</h1>
-          </div>
-        )}
-        <TiddlersList tiddlers={data} />
-        {data.length > tiddlerstore.loadedItems && (
-          <button
-            onClick={handleLoadMore}
-            className="text-sm font-medium text-neutral-600 hover:text-neutral-800 bg-neutral-200 rounded px-2 font-mono py-1"
-          >
-            加载更多
-          </button>
-        )}
-      </div>
-    )
+    <>
+      {/* @ts-ignore */}
+      <CalendarHeatmapComponent datas={tiddlers} />
+      {/* @ts-ignore */}
+      <TiddlersList tiddlers={tiddlers} />
+    </>
   );
 }
--- components/TiddlerItem.tsx
+++ components/TiddlerItem.tsx
@@ -1,6 +1,7 @@
 import { FcFolder } from 'react-icons/fc';
 
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 import Badge from './PostList/PostBadges';
 
@@ -14,13 +15,14 @@ export default function TiddlerItem({
   index: number;
 }) {
   const { title, slug, date } = tiddler;
+  const pathname = usePathname();
   return (
     <li className="ml-6 group my-8">
       <span className="absolute flex items-center justify-center w-6 h-6 rounded-full -left-3 bg-white">
         <FcFolder className="h-4 w-4 text-gray-400 duration-300 transition-all group-hover:stroke-indigo-500" />
       </span>
       <Link
-        href={`/tiddlers/${slug}`}
+        href={`${pathname}/${slug}`}
         className="text-xs rounded-md"
         title="点击阅读全文"
       >
