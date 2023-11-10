21c7f5ccbebf0333a006907134e45118affba96e
oeyoews <jyao4783@gmail.com>
2023-11-10 23:59:08 +0800

    Revert "update"
    This reverts commit 0c4579591d0982ed8f1b738e462b2f51db9f7c58.

--- app/lib/getTiddlerData.ts
+++ app/lib/getTiddlerData.ts
@@ -11,16 +11,16 @@ const TidderJsonFile =
 export default async function getTiddlerData(tiddlerjsonfile = TidderJsonFile) {
   const res = await fetch(tiddlerjsonfile, {
     // 'force-cache' is the default, and can be omitted
-    cache: 'no-store',
+    // cache: 'force-cache',
     // mode: 'cors',
     // method: 'GET',
     // credentials: 'include', // 发送凭据，允许包含 cookie 在内的身份验证信息
     // headers: {
     //   Authorization: '', // 设置自定义的 Authorization 请求头
     // },
-    // next: {
-    //   revalidate: 3600, // In 3600 seconds update
-    // },
+    next: {
+      revalidate: 3600, // In 3600 seconds update
+    },
   });
   if (!res.ok) {
     throw new Error('Failed to fetch data');
