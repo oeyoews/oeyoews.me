5b2b7373ad2ae693909558db9288c6af2a8ddc0b
oeyoews <jyao4783@gmail.com>
2023-11-14 17:43:11 +0800

    fix: remove cache #25

--- app/ui/Viewcounter.tsx
+++ app/ui/Viewcounter.tsx
@@ -1,6 +1,9 @@
+import { unstable_noStore as noStore } from 'next/cache';
+
 import { kv } from '@vercel/kv';
 
 export default async function ViewCounter(params: { slug: string }) {
+  noStore();
   const { slug } = params;
   const views = await kv.incr(slug);
 
