456c572d0f8a99a596e1ae292c8115974ea66b2d
oeyoews <jyao4783@gmail.com>
2023-11-14 12:21:02 +0800

    update

--- app/ui/Nav.tsx
+++ app/ui/Nav.tsx
@@ -1,6 +1,4 @@
-import { FcPlanner } from 'react-icons/fc';
-import { GoIssueOpened } from 'react-icons/go';
-import { SiMdx, SiTiddlywiki } from 'react-icons/si';
+import { BsBook, BsCircle, BsFiletypeMdx, BsWikipedia } from 'react-icons/bs';
 
 import Link from 'next/link';
 
@@ -10,16 +8,16 @@ export default function Nav() {
     <div className="flex items-center justify-between print:hidden mb-8 sticky top-0 left-0 z-[1000] backdrop-blur-sm p-4 bg-white/30 max-w-4xl mx-auto ">
       <nav className="ml-auto text-sm font-medium space-x-6 flex flex-row">
         <Link href="/" title="Home">
-          <FcPlanner className={LinkClass} />
+          <BsBook className={LinkClass} />
         </Link>
         <Link href="/posts" title="Blog">
-          <SiMdx className={LinkClass} />
+          <BsFiletypeMdx className={LinkClass} />
         </Link>
         <Link href="/tiddlers" title="tiddlers">
-          <SiTiddlywiki className={LinkClass} />
+          <BsWikipedia className={LinkClass} />
         </Link>
         <Link href="/github" title="issues">
-          <GoIssueOpened className={`${LinkClass} text-lime-700`} />
+          <BsCircle className={`${LinkClass}`} />
         </Link>
       </nav>
     </div>
