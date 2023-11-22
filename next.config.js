3354b4537fc59bb65098cfc06ff404e7c20efb01
oeyoews <jyao4783@gmail.com>
2023-11-22 23:45:43 +0800

    perf: remove some dependency

--- next.config.js
+++ next.config.js
@@ -1,50 +1,6 @@
-const { withContentlayer } = require('next-contentlayer');
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  async headers() {
-    return [
-      {
-        // matching all API routes
-        source: '/:path*',
-        headers: [
-          // { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          { key: 'Access-Control-Allow-Origin', value: '*' }, // replace this your actual origin
-          {
-            key: 'Access-Control-Allow-Methods',
-            value: 'GET,DELETE,PATCH,POST,PUT',
-          },
-          {
-            key: 'Access-Control-Allow-Headers',
-            value:
-              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
-          },
-        ],
-      },
-    ];
-  },
-  async rewrites() {
-    return [
-      // 似乎/不起作用 , 后面需要加名字
-      {
-        source: '/index',
-        destination: '/tiddlers',
-      },
-      {
-        source: '/rss',
-        destination: '/feed.xml',
-      },
-      {
-        source: '/rss.xml',
-        destination: '/feed.xml',
-      },
-      {
-        source: '/feed',
-        destination: '/feed.xml',
-      },
-    ];
-  },
   pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
   reactStrictMode: false,
   images: {
@@ -62,4 +18,4 @@ const nextConfig = {
   },
 };
 
-module.exports = withContentlayer(nextConfig);
+module.exports = nextConfig;
