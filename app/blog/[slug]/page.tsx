bdb8817eca92722a25cc76123f308c8bb1896027
oeyoews <jyao4783@gmail.com>
2023-11-23 02:10:08 +0800

    update

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -29,21 +29,22 @@ import Spinner from '~ui/Spinner';
 
 function getPostFromParams(slug: string) {
   const post = getBlogPosts().find((post) => post.slug === slug);
-  return post;
+  return {
+    slug: post?.slug,
+  };
 }
 
-// export async function generateMetadata({
-//   params,
-// }: PostProps): Promise<Metadata> {
-//   const post = await getPostFromParams(params);
-//   return {
-//   };
-// }
+export async function generateMetadata({ params }: { params: Params }) {
+  const post = await getPostFromParams(params.slug);
+  return {
+    title: post.slug,
+  };
+}
 
 export async function generateStaticParams() {
-  return getBlogPosts().map(({ slug }) => ({
-    slug,
-  }));
+  const posts = getBlogPosts();
+  // @ts-ignore
+  return posts.map((posts) => ({ slug: posts.metadata.slug }));
 }
 
 export default async function PostPage({ params }: { params: Params }) {
@@ -54,11 +55,13 @@ export default async function PostPage({ params }: { params: Params }) {
 
   return (
     <article className="py-6 prose prose-img:rounded-md">
+      {/* @ts-ignore */}
       <h1 className="mb-2 text-3xl">{post.metadata.title}</h1>
       <hr className="my-4 border-2 border-gray-100 rounded-full" />
       <PasswordProtectedContent post={post}>
         <Suspense fallback={<Spinner center={true} size={88} />}>
           <article>
+            {/* @ts-ignore */}
             <MDX source={post?.content} />
           </article>
         </Suspense>
