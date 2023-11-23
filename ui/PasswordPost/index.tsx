f7be1bac1b62f4dee17e190a7ced968262498f2c
oeyoews <jyao4783@gmail.com>
2023-11-23 18:06:21 +0800

    update

--- ui/PasswordPost/index.tsx
+++ ui/PasswordPost/index.tsx
@@ -4,6 +4,7 @@ import { ReactElement, useState } from 'react';
 import { AiOutlineSend } from 'react-icons/ai';
 
 import useStore from '~lib/store';
+import { DraftSkeleton } from '~ui/Skeleton';
 
 function PasswordProtectedContent({
   post,
@@ -30,31 +31,26 @@ function PasswordProtectedContent({
       {/* 如果其他文章输入密码, 会影响 */}
       {!metadata.password && !metadata.draft && children}
       {metadata.draft && (
-        <div className="prose max-w-none">
-          <small className="fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
-            正在努力编写中 ...
-          </small>
+        <div className="prose max-w-none absolute">
+          <div className="fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+            <p className="text-center">正在努力编写中</p>
+            <DraftSkeleton />
+          </div>
         </div>
       )}
       {metadata.password && !passwordStore.showContent ? (
-        <form
-          className="fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 bg-gray-100 p-1 rounded-md flex mx-2"
-          autoComplete="off"
-        >
+        <form className="flex justify-center items-center" autoComplete="off">
           <label>
             <input
               type="password"
-              className="bg-gray-100 border-none rounded p-2 focus:outline-none mx-2"
+              className="input input-bordered mx-1"
+              autoFocus={true}
               placeholder="password"
-              // value={enteredPassword}
               onChange={(e) => setEnteredPassword(e.target.value)}
             />
           </label>
-          <button
-            onClick={handlePasswordSubmit}
-            className="hidden lg:block mr-2"
-          >
-            <AiOutlineSend className="h-5 w-5 text-gray-400 duration-300 transition-all" />
+          <button onClick={handlePasswordSubmit} className="btn">
+            <AiOutlineSend className="h-5 w-5" />
           </button>
         </form>
       ) : (
---
+++ ui/Skeleton.tsx
@@ -0,0 +1,10 @@
+export const DraftSkeleton = () => {
+  return (
+    <div className="flex flex-col gap-4 w-96">
+      <div className="skeleton h-32 w-full"></div>
+      <div className="skeleton h-4 w-28"></div>
+      <div className="skeleton h-4 w-full"></div>
+      <div className="skeleton h-4 w-full"></div>
+    </div>
+  );
+};
