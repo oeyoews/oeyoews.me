f5657de11cd1b7fb4000f50accccdcee49bbaed9
oeyoews <jyao4783@gmail.com>
2023-11-23 22:50:39 +0800

    feat: support no frontmatter

--- app/blog/blog.ts
+++ app/blog/blog.ts
@@ -10,43 +10,67 @@ type Metadata = {
   draft: boolean | string;
 };
 
-// mdx-remote 也是支持parsefrontmatter, 不过不能直接传到mdxremote 需要compile, 不够方便
-const parseFrontmatter = (fileContent: string) => {
+const parseDate = (dateString: string): string | number => {
+  const parsedDate = Date.parse(dateString);
+  return isNaN(parsedDate) ? dateString : parsedDate;
+};
+const parseFrontmatter = (fileContent: string, fileName: string) => {
   const frontmatterRegex = /---\s*([\s\S]*?)\s*---/;
   const match = frontmatterRegex.exec(fileContent);
-  const frontMatterBlock = match![1];
-  const content = fileContent.replace(frontmatterRegex, '').trim();
-  const frontMatterLines = frontMatterBlock.trim().split('\n');
-  const metadata: Partial<Metadata> = {};
 
-  frontMatterLines.forEach((line) => {
-    const [key, ...valueArr] = line.split(': ');
-    let value: any = valueArr.join(': ').trim();
-    value = value.replace(/^['"](.*)['"]$/, '$1'); // Remove quotes
-    if (value === 'true') {
-      value = true;
-    } else if (value === 'false') {
-      value = false;
-    }
-    metadata[key.trim() as keyof Metadata] = value;
-  });
+  if (match) {
+    const frontMatterBlock = match[1];
+    const content = fileContent.replace(frontmatterRegex, '').trim();
+    const frontMatterLines = frontMatterBlock.trim().split('\n');
+    const metadata: Partial<Metadata> = {};
 
-  return { metadata: metadata as Metadata, content };
+    frontMatterLines.forEach((line) => {
+      const [key, ...valueArr] = line.split(': ');
+      let value: any = valueArr.join(': ').trim();
+      value = value.replace(/^['"](.*)['"]$/, '$1'); // Remove quotes
+      if (value === 'true') {
+        value = true;
+      } else if (value === 'false') {
+        value = false;
+      }
+      if (key.trim() === 'date') {
+        value = parseDate(value);
+      }
+      metadata[key.trim() as keyof Metadata] = value;
+    });
+
+    return { metadata: metadata as Metadata, content };
+  } else {
+    // No frontmatter found, use default values
+    return {
+      metadata: {
+        title: fileName, // Use file name as the default title
+        date: new Date().toISOString(), // Use current date as the default
+        summary: '',
+        image: '',
+        password: '',
+        draft: false,
+      },
+      content: fileContent.trim(),
+    };
+  }
 };
+
 
 const getMDXFiles = (dir: string) => {
   return fs.readdirSync(dir).filter((file) => path.extname(file) === '.mdx');
 };
 
-const readMDXFile = (filePath: string) => {
+const readMDXFile = (fileName: string, dir: string) => {
+  const filePath = path.join(dir, fileName);
   const rawContent = fs.readFileSync(filePath, 'utf-8');
-  return parseFrontmatter(rawContent);
+  return parseFrontmatter(rawContent, fileName);
 };
 
 const getMDXData = (dir: string) => {
   const mdxFiles = getMDXFiles(dir);
   return mdxFiles.map((file) => {
-    const { metadata, content } = readMDXFile(path.join(dir, file));
+    const { metadata, content } = readMDXFile(file, dir);
     const slug = path.basename(file, path.extname(file));
     return {
       metadata,
@@ -55,6 +79,7 @@ const getMDXData = (dir: string) => {
     };
   });
 };
+
 
 export const getBlogPosts = () => {
   const content = process.env.content || 'content';
