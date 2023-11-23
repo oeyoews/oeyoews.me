08b538aece3f6eb3fb7543c469de848e559c1c6b
oeyoews <jyao4783@gmail.com>
2023-11-23 08:47:42 +0800

    update

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,7 +1,6 @@
 import { Suspense } from 'react';
 
 import { Metadata } from 'next';
-import { compileMDX } from 'next-mdx-remote/rsc';
 import { notFound } from 'next/navigation';
 
 import MDX from '~app/blog/Mdx';
@@ -9,42 +8,23 @@ import { getBlogPosts } from '~app/blog/db/blog';
 import PasswordProtectedContent from '~ui/PasswordPost';
 import Spinner from '~ui/Spinner';
 
-// export default function Page({ params }: { params: Params }) {
-//   const { slug } = params;
-//   const post = getBlogPosts().find((post) => post.slug === slug);
-//   if (!post) {
-//     notFound();
-//   }
-//   return (
-//     <div className="prose">
-//       <Suspense fallback={<Spinner center={true} size={88} />}>
-//         <h2>{post?.metadata.title}</h2>
-//         <article>
-//           <MDX source={post?.content} />
-//         </article>
-//       </Suspense>
-//     </div>
-//   );
-// }
+const getPostFromParams = (slug: string) =>
+  getBlogPosts().find((post) => post.slug === slug);
 
-function getPostFromParams(slug: string) {
-  return getBlogPosts().find((post) => post.slug === slug);
-}
-
-export async function generateMetadata({ params }: { params: Params }) {
-  const post = await getPostFromParams(params.slug);
+export const generateMetadata = ({ params }: { params: Params }): Metadata => {
+  const post = getPostFromParams(params.slug);
   return {
     title: post?.slug,
   };
-}
+};
 
-export async function generateStaticParams() {
+export const generateStaticParams = () => {
   const posts = getBlogPosts();
   // @ts-ignore
   return posts.map((posts) => ({ slug: posts.slug }));
-}
+};
 
-export default async function PostPage({ params }: { params: Params }) {
+const PostPage = ({ params }: { params: Params }) => {
   const post = getPostFromParams(params.slug);
   if (!post) {
     notFound();
@@ -65,4 +45,6 @@ export default async function PostPage({ params }: { params: Params }) {
       </PasswordProtectedContent>
     </article>
   );
-}
+};
+
+export default PostPage;
