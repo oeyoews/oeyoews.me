5a946255aa6ea0c1c59f79bdfe9d3225298ed243
oeyoews <jyao4783@gmail.com>
2023-11-27 14:04:06 +0800

    update

--- lib/store/index.ts
+++ lib/store/index.ts
@@ -4,15 +4,12 @@ interface StoreState {
   loadedItems: number;
   enteredPassword: string;
   showContent: boolean;
-  isPlaying: boolean;
-  isClient: boolean;
 }
 
 interface StoreActions {
   setLoadedItems: (loadedItems: number) => void;
   setPassword: (password: string) => void;
   setShowContent: (show: boolean) => void;
-  setIsPlaying: (isPlaying: boolean) => void;
 }
 
 type UseStore = StoreState & StoreActions;
@@ -20,13 +17,10 @@ type UseStore = StoreState & StoreActions;
 const useStore = create<UseStore>()((set) => ({
   loadedItems: 30,
   enteredPassword: '',
-  isPlaying: false,
   showContent: false,
-  isClient: false,
   setLoadedItems: (loadedItems) => set({ loadedItems }),
-  setPassword: (password) => set({ enteredPassword: password }),
+  setPassword: (enteredPassword) => set({ enteredPassword }),
   setShowContent: (show) => set({ showContent: show }),
-  setIsPlaying: (isPlaying) => set({ isPlaying: isPlaying }),
 }));
 
 export default useStore;
