a4b1a4c60262c29796f87559a3bedbdaee685543
oeyoews <jyao4783@gmail.com>
2023-12-03 21:02:09 +0800

    update

--- components/PostList/YearHeader.tsx
+++ components/PostList/YearHeader.tsx
@@ -1,4 +1,7 @@
-import Icon from '../Icon';
+import { FaRegCheckCircle } from 'react-icons/fa';
+
+import Icon from '~components/Icon';
+import Timeline from '~components/Timeline';
 
 // const zodiacs = [
 //   '🐀',
@@ -34,12 +37,7 @@ function YearHeader({ postYear }: { postYear: number }) {
   const animal = (postYear - 4) % 12;
   return (
     <div className=" border-gray-100/80 border-l-2 pb-4 relative">
-      <span className="absolute flex items-center justify-center w-6 h-6 rounded-full -left-[13px] top-1">
-        <Icon
-          icon="gg:check-o"
-          className="h-4 w-4 duration-300 transition-all text-[#8bc34a]"
-        />
-      </span>
+      <Timeline.Dot icon={<FaRegCheckCircle />} />
       <div className="text-xl ml-6 select-none text-neutral-300  dark:text-neutral-800 font-extrabold font-serif my-0">
         {postYear}
         <span title={zodiacs[animal]}>
--- components/Timeline/Dot.tsx
+++ components/Timeline/Dot.tsx
@@ -1,9 +1,14 @@
+import React from 'react';
 import { FaRegDotCircle } from 'react-icons/fa';
 
-export default function Dot() {
+export default function Dot({ icon }: { icon?: any }) {
+  const Icon = icon || <FaRegDotCircle />;
   return (
     <span className="absolute flex items-center justify-center w-6 h-6 rounded-full -left-[13px]">
-      <FaRegDotCircle className="h-4 w-4 text-gray-300/80 duration-300 transition-all group-hover:text-[#8bc34a] group-hover:scale-105" />
+      {React.cloneElement(Icon, {
+        className:
+          'h-4 w-4 text-gray-300/80 duration-300 transition-all group-hover:text-[#8bc34a] group-hover:scale-105',
+      })}
     </span>
   );
 }
