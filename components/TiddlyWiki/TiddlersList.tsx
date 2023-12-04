89d30d70379b226e3a8191cde9f603457d37dce2
oeyoews <jyao4783@gmail.com>
2023-12-04 21:31:03 +0800

    update

--- components/TiddlyWiki/TiddlersList.tsx
+++ components/TiddlyWiki/TiddlersList.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import { useEffect, useState } from 'react';
 import { toast } from 'react-hot-toast';
 
 import TiddlerItem from '~components/TiddlyWiki/TiddlerItem';
@@ -17,68 +16,42 @@ export default function TiddlersList({
 }) {
   const loadedItems = useBlogStore.use.loadedItems();
   const setLoadedItems = useBlogStore.use.setLoadedItems();
-  const [data, setData] = useState<TiddlerMetadata[]>([]);
-  const [searchTerm, setSearchTerm] = useState('');
-  const [hasLoaded, setHasLoaded] = useState(false);
-
-  useEffect(() => {
-    if (tiddlers && tiddlers.length > 0) {
-      setData(tiddlers);
-      setHasLoaded(true);
-    }
-  }, [tiddlers]);
 
   const handleLoadMore = () => {
-    if (data.length < loadedItems) {
+    if (tiddlers.length < loadedItems) {
       toast.error('没有更多了');
       return;
     }
     setLoadedItems(loadedItems + 30);
-    toast.success('加载成功');
   };
-
-  useEffect(() => {
-    if (!searchTerm) {
-      setData(tiddlers);
-      return;
-    }
-    const filteredData = tiddlers.filter((tiddler) =>
-      tiddler.title.toLowerCase().includes(searchTerm.toLowerCase()),
-    );
-    setData(filteredData);
-  }, [searchTerm, tiddlers]);
 
   let currentYear: number;
 
-  const TiddlerListContent = () => (
-    <Timeline>
-      {data.slice(0, loadedItems).map((tiddler, index) => {
-        const { title, date } = tiddler;
-        const postYear = new Date(date).getFullYear();
-        const yearHeader = currentYear !== postYear && (
-          <YearHeader postYear={postYear} />
-        );
-        currentYear = postYear;
-
-        return (
-          <TiddlerItem
-            tiddler={tiddler}
-            index={index}
-            key={title}
-            order={index === data.length - 1 ? 'end' : 'normal'}
-            route={route}
-          >
-            {yearHeader}
-          </TiddlerItem>
-        );
-      })}
-    </Timeline>
-  );
-
   return (
-    <div>
-      <TiddlerListContent />
-      {data.length > loadedItems && (
+    <>
+      <Timeline>
+        {tiddlers.slice(0, loadedItems).map((tiddler, index) => {
+          const { title, date } = tiddler;
+          const postYear = new Date(date).getFullYear();
+          const yearHeader = currentYear !== postYear && (
+            <YearHeader postYear={postYear} />
+          );
+          currentYear = postYear;
+
+          return (
+            <TiddlerItem
+              tiddler={tiddler}
+              index={index}
+              key={title}
+              order={index === tiddlers.length - 1 ? 'end' : 'normal'}
+              route={route}
+            >
+              {yearHeader}
+            </TiddlerItem>
+          );
+        })}
+      </Timeline>
+      {tiddlers.length > loadedItems && (
         <button
           onClick={handleLoadMore}
           className="text-sm font-medium rounded px-2 font-mono py-1"
@@ -86,6 +59,6 @@ export default function TiddlersList({
           加载更多
         </button>
       )}
-    </div>
+    </>
   );
 }
