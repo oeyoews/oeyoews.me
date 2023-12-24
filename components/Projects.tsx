4730af2b167c66caaac18ed757c6b38f042a557c
oeyoews <jyao4783@gmail.com>
2023-12-24 17:16:33 +0800

    update

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,10 +1,10 @@
 import { Article } from './ArticleComponents';
 import MarkdownItRenderer from './MarkdownIt';
 
+import config from '~config';
+
 export default async function Projects() {
-  const res = await fetch(
-    'https://cdn.jsdelivr.net/gh/oeyoews/oeyoews@main/README.md',
-  );
+  const res = await fetch(config.projects);
   const data = await res.text();
 
   return (
