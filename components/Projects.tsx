a4cbb5e504b6610f6393b724a0aaaedacaba3bd1
oeyoews <jyao4783@gmail.com>
2023-12-24 17:35:16 +0800

    update

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -4,6 +4,7 @@ import MarkdownItRenderer from './MarkdownIt';
 import config from '~config';
 
 export default async function Projects() {
+  if (process.env.NODE_ENV === 'development') return <>本地不可见</>;
   const res = await fetch(config.projects);
   const data = await res.text();
 
