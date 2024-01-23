05486fa2852370db19522cebe7abdc2bd9c48159
oeyoews <jyao4783@gmail.com>
2024-01-24 01:29:35 +0800

    update

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true
+  },
   reactStrictMode: false,
   pageExtensions: ['ts', 'tsx', 'js', 'jsx'],
   reactStrictMode: false,
